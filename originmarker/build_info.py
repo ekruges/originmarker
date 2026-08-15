e8c2f9d00a3d5841840c9065a47e39e02b5f2695
Ezra Kruger <kruger.ezra.s@gmail.com>
2026-08-15 11:24:37 -0400

    build: 4.12.1

--- originmarker/build_info.py
+++ originmarker/build_info.py
@@ -56,7 +56,7 @@ RELEASES: tuple[Release, ...] = (
             "can stop predicting the other."),
 )
 
-VERSION = "4.12.0"
+VERSION = "4.12.1"
 CODENAME = "Tetrad"
 
 BUILD = f'Build {VERSION} "{CODENAME}"'
